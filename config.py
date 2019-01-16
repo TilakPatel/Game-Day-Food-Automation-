b572acab24606b4f50505182bc8a8755f6b52d29
Tilak Patel <ptilak142@gmail.com>
2019-01-16 15:51:34 -0500

    progress e

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-credentials = {'firstName': 'Tilak', 'lastName': 'Patel', 'email': 'ptilak142@gmail.com', 'addressLine': '69 Hawthorne Village Rd.', 'city': 'Nashua', 'state': 'NH', 'zip': '03062'}
+credentials = {'firstName': 'Tilak', 'lastName': 'Patel', 'phone': '6038888357', 'email': 'ptilak142@gmail.com', 'addressLine': '69 Hawthorne Village Rd.', 'city': 'Nashua', 'state': 'NH', 'zip': '03062'}
+items = ['B8PCPT', '12SCREEN'] #bread sticks and med pizza
+card = {'number': '4100123422343234', 'expiration': '0115', 'cvv': '0115', 'zip': '90210'}
--- script.py
+++ script.py
@@ -1,55 +1,63 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from pizzapi.pizzapi import *
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from pizzapi import *
 import config as credentials
 import datetime
 import time
-# now = datetime.datetime.now()
-# WINDOW_SIZE = "1920,1080"
+WINDOW_SIZE = "1920,1080"
 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-# chrome_options.add_argument('log-level=3')
-# # driver = webdriver.Chrome()
-# driver = webdriver.Chrome(chrome_options=chrome_options)
-# dayLastOrdered = ''
-# while True:
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('log-level=3')
+driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome()
+dayLastOrdered = ''
+while True:
+    now = datetime.datetime.now()
+    driver.get("https://www.nba.com/celtics/schedule")
+    while True:
+        day = now.strftime("%d")
+        if (int(day) < 10):
+            day = day.replace("0", "")
+        currentDate = now.strftime("%b ") + day
+        if (currentDate != dayLastOrdered):
+            try:
+                tvProvs = driver.find_element_by_css_selector(
+                    "div[data-shortdate='" + currentDate + "'").get_attribute('data-tv')
+                pizzaShouldBeOrdered = (("ESPN" in tvProvs) or (
+                    "ABC" in tvProvs) or ("TNT" in tvProvs))
+                timeOfGamehelper = driver.find_element_by_css_selector(
+                    "div[data-shortdate='" + currentDate + "'").get_attribute('data-time')
+                timeOfGame = (str(timeOfGamehelper)[:timeOfGamehelper.find(':')])
+                print(timeOfGame)
+                if (pizzaShouldBeOrdered):
+                    customer = Customer(credentials.credentials['firstName'], credentials.credentials['lastName'],
+                                        credentials.credentials['email'], credentials.credentials['phone'])
+                    address = Address(credentials.credentials['addressLine'], credentials.credentials['city'],
+                                      credentials.credentials['state'], credentials.credentials['zip'])
+                    store = address.closest_store()
+                    order = Order(store, customer, address)
+                    for item in credentials.items:
+                        order.add_item(item)
+                    card = PaymentObject(
+                        credentials.card['number'], credentials.card['expiration'], credentials.card['cvv'], credentials.card['zip'])
+                    orderText = order.pay_with(card)
+                    timeEstimate = (orderText['Order']['EstimatedWaitMinutes'])
+                    minutesLeft = (str(timeEstimate)[timeEstimate.find('-') + 1:])
+                    print('You paid $',
+                          orderText['Order']['Amounts']['Payment'])
+                    dayLastOrdered = currentDate
+                    
+                else:
+                    print('no game today.')
+            except:
+                print('No game today.')
+        time.sleep(10800)  # repeats every 3 hours
+    # refresh calendar data every 1 hour just in case games switch up
+    time.sleep(3600)
 
-#     driver.get("https://www.nba.com/celtics/schedule")
-#     while True:
-#         day = now.strftime("%d")
-#         if (int(day) < 10):
-#             day = day.replace("0", "")
-#         currentDate = now.strftime("%b ") + day
-#         if (currentDate != dayLastOrdered):
-#             try:
-#                 tvProvs = driver.find_element_by_css_selector(
-#                     "div[data-shortdate='" + currentDate + "'").get_attribute('data-tv')
-#                 pizzaShouldBeOrdered = (("ESPN" in tvProvs) or (
-#                     "ABC" in tvProvs) or ("TNT" in tvProvs))
-#                 if (pizzaShouldBeOrdered):
-#                     customer = Customer('Donald', 'Trump',
-#                                         'donald@whitehouse.gov', '2024561111')
-#                     address = Address('700 Pennsylvania Avenue NW', 'Washington', 'DC', '20408')
-#                     store = address.closest_store()
-#                     menu = store.get_menu()
-#                     menu.search(Name='Coke')
-#                 else:
-#                     print('neet')
-#             except:
-#                 print('No game today.')
-#         time.sleep(120)  # repeats every two minutes
-#     # refresh calendar data every 1 hour just in case games switch up
-#     time.sleep(3600)
-
-
-# # driver.close()
-# customer = Customer('Tilak', 'Patel',
-#                     'donald@whitehouse.gov', '2024561111')
-# address = Address('700 Pennsylvania Avenue NW', 'Washington', 'DC', '20408')
-# store = address.closest_store()
-# menu = store.get_menu()
-# menu.search(Name='Coke')
-# print(customer)
-print(credentials.credentials['city'])
+# driver.close()
+# a = datetime.datetime.now().hour
+# print(a) 
+#only order if a is equal to gametime - 1
